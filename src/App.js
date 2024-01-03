4d8f9bcfdaa754f975c590714af58d498aff37e2
Kelly Schumacher <kschumacher93@gmail.com>
2024-01-03 16:40:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={process.env.PUBLIC_URL + '/favicon.ico'} alt="Jolt Pay Logo" />
+        <h1>Jolt Pay</h1>
       </header>
     </div>
   );
